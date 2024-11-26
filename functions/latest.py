7d87acfd9c5cf5004518728292ccf36620cb9d7c
fysalkasim <fysalkasim@gmail.com>
2024-11-26 18:56:48 +0530

    functions

--- functions/latest.py
+++ functions/latest.py
@@ -82,3 +82,58 @@
 #  #global
 # year = 2026
 # print(year)
+
+# Assignment 71:
+# Write a function named get_formatted_name() that takes three parameters: first_name, 
+# last_name, and an optional middle_name. The function should return the full name, neatly formatted, 
+# with the first and last name always included, and the middle name only if it's provided. Then, use this function
+
+# def get_formatted_name():
+#     first_name = input("Enter the first name: ")
+#     last_name = input("Enter the middle name: ")
+#     print(f"you are {first_name} {last_name}")
+
+# # get_formatted_name()
+
+# def create_student_profile(first_name, last_name,**userinfo):
+#     # print(f"I think you are name is {first_name} {last_name}")
+
+#     userinfo.update({"First_name":first_name,"last_name":last_name})
+#     print(userinfo)
+
+
+# create_student_profile("fysal","kasim",place="Kaloor")
+
+# s = sum((1,2),100)
+# print(s)
+
+
+
+# name = "hey "
+# # print(dir(name))
+
+# list1 = ["Fysal",2,3,4,5,6,7]
+# list2 = [80,6,7,8,9,10]
+# print(dict(zip(list1,list2)))
+
+# print(dict([("A","C")]))
+
+# str = ["apple","orange","banana"]
+# # Calling function
+# sorted1 = sorted(str) # sorting string
+# # Displaying result
+# print(sorted1)
+# x = 5
+# print('x =', x, '= y')
+
+# print(round(10.066,2))
+# \
+# 7
+
+# mydoubler = myfunc(2)
+# print(mydoubler(2))
+
+# mytripller = myfunc(3)
+# print(mytripller(2))
+
+
