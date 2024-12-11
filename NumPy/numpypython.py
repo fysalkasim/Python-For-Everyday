33afe1b992f866bb2eab1c4b156a8a44dc765046
fysalkasim <fysalkasim@gmail.com>
2024-12-11 11:11:42 +0530

    numpy

--- NumPy/numpypython.py
+++ NumPy/numpypython.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 # # import time
 # # a = np.array([1,2,3])  #one dimensional. #two dimesional
 # # print(a)
@@ -78,3 +78,85 @@
 # arr1 = np.array([[1,2,3,4,6,7,8,9,1,2,3,4,5,65,1,2]])
 
 # print(arr1.reshape(2,2,2,2))
+
+# x = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]])
+# print('Our array is:')
+# print(x)
+# print('\n')
+
+# rows = np.array([[0, 0], [3, 3]])
+# cols = np.array([[0, 2], [0, 2]])
+# y = x[[[0, 0], [3, 3]], [[0, 2], [0, 2]]]
+# print(y)
+
+# a = np.arange(0,60,5)
+# a = a.reshape(3,2,2)
+
+# print(a)
+# for i in a:
+#     for j in i:
+#         for k in j:
+#             print(k)
+
+# a = np.array([[[1,2][3,4][4,6]]])
+
+# print('First array:')
+# print(a)
+
+# b = np.array([[5,6],[7,8]])
+
+# print('Second array:')
+# print(b)
+
+# # both the arrays are of same dimensions
+
+# print('Joining the two arrays along axis 0:')  #
+# print(np.concatenate((a, b)))
+
+# print('Joining the two arrays along axis 1:')
+# print(np.concatenate((a, b), axis=1))
+
+
+# bc = np.arange(1,31).reshape(10,3)
+# print(bc.shape)
+
+
+
+
+a = np.array([5,2,6,2,7,5,6,8,2,9])
+#[2 5 6 7 8 9]
+# print('First array:')
+# print(a)
+# print('\n')
+
+print('Unique values of first array:')
+u = np.unique(a)
+print(u)
+print('\n')
+
+print('Unique array and Indices array:')
+u,indices = np.unique(a, return_index = True)
+print(indices)
+print('\n')
+
+# print('We can see each number corresponds to index in original array:')
+# print(a)
+# print('\n')
+
+print('Inverses of unique array:')
+u,inverse = np.unique(a,return_inverse = True)
+# print(u)
+# print('\n')
+
+# print('Indices are:')
+print(inverse)
+# print('\n')
+
+# print('Reconstruct the original array using indices:')
+# print(u[indices])
+# print('\n')
+
+# print('Return the count of repetitions of unique elements:')
+# u,indices = np.unique(a,return_counts = True)
+# print(u)
+# print(indices)
---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello world")
+print("I am modified")
+print("Welcome back")
