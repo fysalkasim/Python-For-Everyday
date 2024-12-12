681dea8f3f5c4a2356bea0a0a06f2bcc97cbe9b9
fysalkasim <fysalkasim@gmail.com>
2024-12-12 11:04:06 +0530

    variables

---
+++ Variables/variables_001.py
@@ -0,0 +1,37 @@
+print("hello world")
+print("welcome to varaiables")
+
+#What is variable??
+#Why we are using it?
+
+student1 = "Adarsh"
+student1 = "Anoop"
+student2 = "Nithin"
+year = 2024
+# print("Welcome", student1,"and",student2, "The current year is ",year)
+
+#rules for a variable
+
+#letters(A-Z,a-z,0-9,Underscores)
+#Special Characters (!@#$%^&*()) are not allowed for this
+#starting letter or underscore
+
+course_name = "Data science"
+print(course_name)
+
+course_yearDec = "2024"
+
+# course_yearDec(course_yearDec)
+
+# print = "Anoop"
+# print(print)
+# print("My print function is not working")
+
+
+x = 6
+print(x)
+# deleting a variable.
+del x    
+print(x)
+
+
