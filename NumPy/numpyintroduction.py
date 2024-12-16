b9984ca724602ec660f242c957e33edcd20519f5
fysalkasim <fysalkasim@gmail.com>
2024-12-16 12:02:53 +0530

    sets

--- NumPy/numpyintroduction.py
+++ NumPy/numpyintroduction.py
@@ -43,3 +43,103 @@
 # print(x)
 # print(x.reshape(2,5))
 # print(np.reshape(x,[2,5]))
+
+
+# import numpy as np
+# a = np.array([[30,40,70],[80,20,10],[50,90,60]])
+
+
+# print('Our array is:')
+# print(a)
+# print('\n')
+
+# print('Applying percentile() function:')
+# print(np.percentile(a, 50))
+# print('\n')
+# #10,20,20,40,50,60,70,80,90
+# print('Applying percentile() function along axis 1:')
+# print(np.percentile(a, 50, axis=1))
+# print('\n')
+
+# print('Applying percentile() function along axis 0:')
+# print(np.percentile(a, 50, axis=0))
+
+
+# import numpy as np
+# a = np.array([[30, 40, 70], [80, 20, 10], [50, 90, 60]])
+# print('Our array is:')
+# print(a)
+# print('\n')
+# print('Applying argmax() function:')
+# print(np.argmax(a))
+# print('\n')
+
+# print('Index of maximum number in flattened array')
+# print(a.flatten())
+# print('\n')
+
+# print('Array containing indices of maximum along axis 0:')
+# maxindex = np.argmax(a, axis=0)
+# print(maxindex)
+# print('\n')
+
+# print('Array containing indices of maximum along axis 1:')
+# maxindex = np.argmax(a, axis=1)
+# print(maxindex)
+# print('\n')
+
+# print('Applying argmin() function:')
+# minindex = np.argmin(a)
+# print(minindex)
+# print('\n')
+
+# print('Flattened array:')
+# print(a.flatten())
+# print('\n')
+
+# print('Flattened array along axis 0:')
+# minindex = np.argmin(a, axis=0)
+# print(minindex)
+# print('\n')
+
+# print('Flattened array along axis 1:')
+# minindex = np.argmin(a, axis=1)
+# print(minindex)
+
+import numpy as np
+# print(np.average([1, 2, 3, 4], weights=[4, 3, 2, 1], returned=False))
+
+# wt = np.array([3,5])
+# a = np.arange(6).reshape(3,2)
+# print(np.average(a, axis=1, weights=wt))
+# print('\n')
+
+
+# print('Modified array:')
+# print(np.average(a, axis=1, weights=wt, returned=False))
+
+
+import numpy as np
+# a = np.array([[30,40,0],[0,20,10],[50,0,60]])
+# print('Our array is:')
+# print(a)
+# print('\n')
+# print('Applying nonzero() function:')
+# c = np.nonzero(a)   #index of nonzero element
+# print(a[c])
+
+
+# a = np.array([[3, 7], [9, 1]])
+# print('Our array is:')
+# print(a)
+# print('Applying sort() function:')
+# print(np.sort(a))
+# print('Sort along axis 0:')
+# print(np.sort(a, axis=0))
+# print("AXIS 1")
+# print(np.sort(a, axis=1))
+
+
+# import numpy.matlib
+# import numpy as np
+# print(np.matlib.eye(n=3, M=3, k=0, dtype=float))
--- Set/setsofpython.py
+++ Set/setsofpython.py
@@ -15,3 +15,27 @@
 # print(type(emptyset))
 
 
+# thisset = {"apple", "banana", "cherry"}
+# thisset.add("orange")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.remove("banadsf")
+# print(thisset)
+
+# fruits = ["apple", "banana", "cherry"]  
+# myfruits = fruits.copy()
+# del fruits  #delteing element form last
+# print(myfruits)
+
+
+# myeptylist = []
+# myeptyset = set()
+# print(type(myeptylist))
+
+# x = {"a", "b", "c"}
+# y = {"c", "b", "a","s"}
+
+# print()
+
+
